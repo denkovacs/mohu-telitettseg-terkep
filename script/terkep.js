894853d9a1d6c3195cbfc44eea8dc048cae53ec1
denkovacs <denkovacs@external.mol.hu>
2025-09-10 10:23:25 +0200

    Add files via upload

--- script/terkep.js
+++ script/terkep.js
@@ -19,8 +19,7 @@ function icoonByCapacity(value) {
         iconUrl = "./img/orange.png";
     } else if (value >= 150) {
         iconUrl = "./img/red.png";
-    }
-
+}
     return new L.Icon({
         iconUrl: iconUrl,
         iconSize: [18.75, 30.75],
@@ -50,7 +49,7 @@ function groupByCoordinates(data) {
     return grouped;
 }
 
-function renderMarkers(typeFilter){
+function renderMarkers(kategoriaFilter="", cikkszamFilter=""){
     markers.forEach(marker=>map.removeLayer(marker));
     markers.length=0;
     markerClusterGroup.clearLayers();
@@ -59,7 +58,10 @@ function renderMarkers(typeFilter){
 
     Object.entries(groupedData).forEach(([coord, locs]) => {
         
-         if (typeFilter && !locs.some(loc => loc.kategoria === typeFilter)) {
+         if (kategoriaFilter && !locs.some(loc => loc.kategoria === kategoriaFilter)) {
+            return;
+        }
+        if (cikkszamFilter && !locs.some(loc => loc.cikkszam === cikkszamFilter)) {
             return;
         }
         const [x, y] = coord.split(", ").map(c => parseFloat(c.trim()));
@@ -72,13 +74,21 @@ function renderMarkers(typeFilter){
 
         const marker =L.marker([x,y], {icon:icoonByCapacity(capacity)});
 
-        marker.bindTooltip(`Kihasználtság: ${locs[0].teljes_kapacitaas}`, {
-            permanent: false,
-            direction: "bottom"
-        });
-
         marker.on('click',()=>{
-            infoPlaceholder.innerHTML= generateInfoHTML(locs);
+
+            const selectedKategoria=kategoriaSelect.value;
+            const selectedCikkszam=cikkszamSelect.value;
+
+            let filteredLocs=locs;
+
+            if (selectedKategoria) {
+                filteredLocs = filteredLocs.filter(loc => loc.kategoria === selectedKategoria);
+            }
+            if (selectedCikkszam) {
+                filteredLocs = filteredLocs.filter(loc => loc.cikkszam === selectedCikkszam);
+            }
+
+            infoPlaceholder.innerHTML= generateInfoHTML(filteredLocs);
             });
             markerClusterGroup.addLayer(marker);
             markers.push(marker);  
@@ -92,10 +102,19 @@ fetch('./adatok.json')
     renderMarkers();
 });
 
-document.getElementById("kategoria-filter").addEventListener("change", function () {
-    const selected = this.value;
-    renderMarkers(selected);
-});
+const kategoriaSelect = document.getElementById("kategoria-filter");
+const cikkszamSelect = document.getElementById("cikkszam-filter");
+
+function updateFilters() {
+    const selectedKategoria = kategoriaSelect.value;
+    const selectedCikkszam = cikkszamSelect.value;
+    renderMarkers(selectedKategoria, selectedCikkszam);
+}
+
+kategoriaSelect.addEventListener("change", updateFilters);
+cikkszamSelect.addEventListener("change", updateFilters);
+
+
 
 map.on('click',()=>{
 infoPlaceholder.innerHTML='<p class="info-placeholder-katt">Kattints egy pontra a részletekért.</p>'
@@ -114,10 +133,11 @@ function generateInfoHTML(locs) {
     Object.entries(groupedByPartner).forEach(([partner, items]) => {
         const varos = items[0].varos ? ` (${items[0].varos})` : '';
         const telitettseg = items[0].teljes_kapacitaas ? items[0].teljes_kapacitaas : '';
+        const keszletdatum = items[0].keszletdatum ? items[0].keszletdatum :'';
         html += `<div class="info-block">
-                    <h2><strong>${partner}${varos}| Kihasználtság: ${telitettseg}</strong></h2>`;
+                    <h2><strong>${partner}${varos}<br>Teljes Kihasználtság: ${telitettseg}</strong> <br>Készlet dátum: ${keszletdatum}</strong></h2>`;
         items.forEach(item => {
-            html += `<p>Cikkszám: <strong>${item.cikkszam}</strong> | Készlet dátum: <strong>${item.keszletdatum}</strong></p>`;
+            html += `<p>Cikkszám: <strong>${item.cikkszam}</strong> | Kihasználtság: <strong>${item.tarolt_telitetseg_cikk}</strong></p>`;
         });
         html += `</div><hr>`;
     });
